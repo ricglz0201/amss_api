bb6c9a4ab7770645921fc274f0ffc38cb4bb11ec
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-05-03 22:13:39 -0700

    Fix typo in reference to reservation

--- db/migrate/20190504043017_create_trips.rb
+++ db/migrate/20190504043017_create_trips.rb
@@ -2,7 +2,7 @@ class CreateTrips < ActiveRecord::Migration[5.2]
   def change
     create_table :trips do |t|
       t.references :seats, foreign_key: true
-      t.references :reservation, foreign_key: true
+      t.references :reservations, foreign_key: true
       t.datetime :date
       t.timestamps
     end
