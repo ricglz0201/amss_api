12f401b1f502d01052126f561b7fa293ef652973
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-05-03 20:27:48 -0700

    Create join table between buses and routes and fix typo in buses_routes migration

--- db/migrate/20190504030940_create_join_table_bus_routes.rb
+++ db/migrate/20190504030940_create_join_table_bus_routes.rb
@@ -2,7 +2,7 @@ class CreateJoinTableBusRoutes < ActiveRecord::Migration[5.2]
   def change
     create_table :bus_routes do |t|
       t.references :buses, foreign_key: true
-      t.references :rotues, foreign_key: true
+      t.references :routes, foreign_key: true
     end
   end
 end
