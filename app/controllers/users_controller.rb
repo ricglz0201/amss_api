b14aaf43d1695eb05e9568dbc3c3ff5fefda9451
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-05-05 12:25:40 -0700

    It is looking through the id for user instead of uid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # Controller for users
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:uid])
+    @user = User.find(params[:id])
     render json: @user
   end
 end
